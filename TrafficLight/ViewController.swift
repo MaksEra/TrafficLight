395780b58375e185e60963a4735d2d9a0f58c3fe
Maks Era <maks9306@gmail.com>
2021-05-19 19:58:55 +0300

    add traffic light viewbox, add button text changes

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
+enum TrafficLights {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    @IBOutlet weak var actionButton: UIButton!
+    
+    private let LightOn: CGFloat = 1
+    private let LightOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        actionButton.layer.cornerRadius = 15
+        redLight.layer.cornerRadius = 50
+        yellowLight.layer.cornerRadius = 50
+        greenLight.layer.cornerRadius = 50
+        
+        redLight.alpha = LightOff
+        yellowLight.alpha = LightOff
+        greenLight.alpha = LightOff
     }
-
+    
+    @IBAction func actionButton(_ sender: Any) {
+        actionButton.setTitle("Next", for: .normal)
+        
+        if redLight.alpha == LightOff {
+            redLight.alpha = LightOn
+            greenLight.alpha = LightOff
+        } else if yellowLight.alpha == LightOff {
+            redLight.alpha = LightOff
+            yellowLight.alpha = LightOn
+        } else if greenLight.alpha == LightOff {
+            yellowLight.alpha = LightOff
+            greenLight.alpha = LightOn
+        }
+        
+    }
 
 }
 
